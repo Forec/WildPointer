19722ddf2af6c01d43447a5af62b7879878b2e51
Forec <forec@bupt.edu.cn>
2017-09-14 14:09:27 +0800

    add acceptance

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-cd /root/work/wp
-source /root/work/wp/venv/bin/activate
+source venv/bin/activate
 gunicorn -w 4 -b 127.0.0.1:5002 --worker-connections 100 wsgi:app
--- settings/run.bat
+++ settings/run.bat
@@ -1,7 +1,4 @@
 cd ../venv/Scripts
-
 activate.bat
-
-
 cd ../..
 gunicorn -w 4 -b 127.0.0.1:5002 --worker-connections 100 wsgi:app
